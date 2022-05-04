1815446d9201d1ece8c72e8c523b5aa959dbb74b
luisjosecruz <luisjosecruzmart@gmail.com>
2022-05-04 16:31:42 -0600

    Home blog

---
+++ blog/index.php
@@ -0,0 +1,161 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, user.scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta name="description" content="Portafolio de Luis José Cruz Martínez">
+    <meta name="keywords" content="developer, desarrollador, web, programmer">
+    <meta name="author" content="Luis José Cruz"> 
+    <meta name="copyright" content="Luis Cruz 2021">
+    <meta name="robots" content="index/follow">
+    <meta http-equiv="cache-control" content="no-cache">
+    <meta http-equiv="expires" content="0">
+    <meta http-equiv="refresh" content="3600">
+    <title>Luis Cruz</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@700&family=Rubik:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+    <link rel="shortcut icon" href="assets/images/icons/favicon.png" type="image/png">
+    <link rel="stylesheet" href="assets/css/main.css">
+    <link rel="stylesheet" href="assets/css/tablet.css" media="(min-width: 768px)">
+    <link rel="stylesheet" href="assets/css/desktop.css" media="(min-width: 1024px)">
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-Q891L0CZCS"></script>
+    <script>window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', 'G-Q891L0CZCS');</script>
+</head>
+<body>
+    <header>
+        <!-- <div class="bg-logo">
+            <div class="tooltifix">
+                <ul>
+                    <li><a href="#">Development</a></li>
+                    <li><a href="#">UI/UX</a></li>
+                    <li><a href="#">Landing Page</a></li>
+                    <li><a href="#">Website </a></li>
+                    <li><a href="#">Mobile Design</a></li>
+                </ul>
+            </div>
+            <img class="logo" src="assets/images/icons/blog.png" alt="Luis Mart Profile Picture">
+        </div> -->
+        <div class="intro">
+            <h1>Una filosofía de vida</h1>
+            <p>Aquí vas a encontrar ideas, soluciones, herramientas e inspiración para crear y dejar tu huella en el mundo.
+            </p>
+        </div>
+        <div class="Firefly">
+            <div class="CurrenciesFirefly"></div><div class="CurrenciesFirefly"></div><div class="CurrenciesFirefly"></div><div class="CurrenciesFirefly"></div><div class="CurrenciesFirefly"></div><div class="CurrenciesFirefly"></div><div class="CurrenciesFirefly"></div><div class="CurrenciesFirefly"></div><div class="CurrenciesFirefly"></div><div class="CurrenciesFirefly"></div><div class="CurrenciesFirefly"></div><div class="CurrenciesFirefly"></div><div class="CurrenciesFirefly"></div><div class="CurrenciesFirefly"></div><div class="CurrenciesFirefly"></div><div class="CurrenciesFirefly"></div><div class="CurrenciesFirefly"></div><div class="CurrenciesFirefly"></div><div class="CurrenciesFirefly"></div><div class="CurrenciesFirefly"></div><div class="CurrenciesFirefly"></div><div class="CurrenciesFirefly"></div><div class="CurrenciesFirefly"></div><div class="CurrenciesFirefly"></div><div class="CurrenciesFirefly"></div><div class="CurrenciesFirefly"></div><div class="CurrenciesFirefly"></div><div class="CurrenciesFirefly"></div><div class="CurrenciesFirefly"></div><div class="CurrenciesFirefly"></div><div class="CurrenciesFirefly"></div><div class="CurrenciesFirefly"></div><div class="CurrenciesFirefly"></div><div class="CurrenciesFirefly"></div><div class="CurrenciesFirefly"></div><div class="CurrenciesFirefly"></div><div class="CurrenciesFirefly"></div><div class="CurrenciesFirefly"></div><div class="CurrenciesFirefly"></div><div class="CurrenciesFirefly"></div><div class="CurrenciesFirefly"></div><div class="CurrenciesFirefly"></div><div class="CurrenciesFirefly"></div><div class="CurrenciesFirefly"></div><div class="CurrenciesFirefly"></div><div class="CurrenciesFirefly"></div><div class="CurrenciesFirefly"></div><div class="CurrenciesFirefly"></div><div class="CurrenciesFirefly"></div><div class="CurrenciesFirefly"></div><div class="CurrenciesFirefly"></div><div class="CurrenciesFirefly"></div><div class="CurrenciesFirefly"></div><div class="CurrenciesFirefly"></div><div class="CurrenciesFirefly"></div><div class="CurrenciesFirefly"></div><div class="CurrenciesFirefly"></div><div class="CurrenciesFirefly"></div><div class="CurrenciesFirefly"></div><div class="CurrenciesFirefly"></div><div class="CurrenciesFirefly"></div><div class="CurrenciesFirefly"></div><div class="CurrenciesFirefly"></div><div class="CurrenciesFirefly"></div><div class="CurrenciesFirefly"></div><div class="CurrenciesFirefly"></div><div class="CurrenciesFirefly"></div><div class="CurrenciesFirefly"></div><div class="CurrenciesFirefly"></div><div class="CurrenciesFirefly"></div><div class="CurrenciesFirefly"></div><div class="CurrenciesFirefly"></div><div class="CurrenciesFirefly"></div><div class="CurrenciesFirefly"></div><div class="CurrenciesFirefly"></div><div class="CurrenciesFirefly"></div><div class="CurrenciesFirefly"></div><div class="CurrenciesFirefly"></div><div class="CurrenciesFirefly"></div><div class="CurrenciesFirefly"></div><div class="CurrenciesFirefly"></div><div class="CurrenciesFirefly"></div><div class="CurrenciesFirefly"></div><div class="CurrenciesFirefly"></div><div class="CurrenciesFirefly"></div><div class="CurrenciesFirefly"></div><div class="CurrenciesFirefly"></div><div class="CurrenciesFirefly"></div><div class="CurrenciesFirefly"></div><div class="CurrenciesFirefly"></div><div class="CurrenciesFirefly"></div><div class="CurrenciesFirefly"></div><div class="CurrenciesFirefly"></div><div class="CurrenciesFirefly"></div><div class="CurrenciesFirefly"></div><div class="CurrenciesFirefly"></div><div class="CurrenciesFirefly"></div><div class="CurrenciesFirefly"></div><div class="CurrenciesFirefly"></div><div class="CurrenciesFirefly"></div>
+        </div>
+    </header>
+    <nav class="navbar">
+        <!-- <ul>
+            <li><a href="#">✨</a></li> 
+            <li><a href="#">🎧</a></li>
+            <li><a href="#">🎓</a></li>
+            <li><a href="#">💻</a></li>
+            <li><a href="#">🖐</a></li>
+        </ul> -->
+        <ul>
+            <li><a class="start" href="https://www.luisjosecruz.com"><img src="assets/images/icons/favicon.png" alt="Luis Mart Logo"> Luis José Cruz</a></li> 
+        </ul>
+    </nav>
+    <a class="contact-link" href="/luisjosecruz">✨ Inicio</a>
+    <main>
+        <section class="proyects">
+            <!-- <div class="section__title"></div> -->
+            <div class="cards_container cards_container_proyects">
+                <div class="card">
+                    <div class="card__content">
+                        <div class="card__img gesproin"></div>
+                        <h3>Desarrollemos nuestra propia filosofía de vida</h3>
+                        <div class="card__tags">
+                            <span class="card__tag">FILOSOFÍA</span>
+                        </div>
+                        <p class="card__text">
+                            La empresa Gesproin S.A de C.V me contrató para que diseñara y desarrollara su sitio web oficial.
+                        </p>
+                    </div>
+                    <a class="card__link" href="luisjosecruz/blog/randowmtext" target="_blank">Leer más</a>
+                </div>
+                <div class="card">
+                    <div class="card__content">
+                        <div class="card__img luismartmusic"></div>
+                        <h3>Desarrollemos nuestra propia filosofía de vida a travéz de la música</h3>
+                        <div class="card__tags">
+                            <span class="card__tag">MÚSICA</span>
+                        </div>
+                        <p class="card__text">
+                            Como hobbie me gusta enfocar mi creatividad para crear música y me he creado un sitio web.
+                        </p>
+                    </div>
+                    <a class="card__link" href="https://luismart.netlify.app/" target="_blank">Leer más</a>
+                </div>
+                <div class="card">
+                    <div class="card__content">
+                        <div class="card__img sersetronica"></div>
+                        <h3>Desarrollemos nuestra propia filosofía de vida</h3>
+                        <div class="card__tags">
+                            <span class="card__tag">DESARROLLO</span>
+                        </div>
+                        <p class="card__text">
+                            Estoy desarrollando un sitio web para una empresa de seguridad electrónica.
+                        </p>
+                    </div>
+                    <a class="card__link" href="https://luisjosecruz.github.io/sersetronica/" target="_blank">Leer más</a>
+                </div> 
+                <div class="card">
+                    <div class="card__content">
+                        <div class="card__img clarkode"></div>
+                        <h3>Desarrollemos nuestra propia filosofía de vida</h3>
+                        <div class="card__tags">
+                            <span class="card__tag">DESARROLLO WEB</span>
+                        </div>
+                        <p class="card__text">
+                            Además me gusta compartir contenido relacionado con la tecnología en YouTube.
+                        </p>
+                    </div>
+                    <a class="card__link" href="https://www.youtube.com/channel/UCqkxFr53JmfLi_NiteA8HHA?sub_confirmation=1" target="_blank">Leer más</a>
+                </div>
+                <!-- <div class="card">
+                    <div class="card__content">
+                        <div class="card__img textract"></div>
+                        <h3>AWS textract</h3>
+                        <div class="card__tags">
+                            <span class="card__tag">Lambda</span>
+                            <span class="card__tag">Phyton</span>
+                            <span class="card__tag">S3</span>
+                            <span class="card__tag">PHP</span>
+                            <span class="card__tag">MYSQL</span>
+                            <span class="card__tag">JS</span> 
+                        </div>
+                        <p class="card__text">
+                            He creado una aplicación web que usa servicios de AWS para extraer texto de imágenes y archivos pdf.
+                        </p>
+                    </div>
+                </div> -->
+            </div>
+        </section>
+        <section class="contact">
+            <div class="contact__text">
+                <h3>Contacto</h3>
+                <p>luisjosecruzmart@gmail.com</p>
+            </div>
+            <div class="contact__img"></div>
+        </section>
+    </main>
+    <footer>
+        <ul class="social-media">
+            <li class="social-media__item"><a href="https://github.com/luisjosecruz" target="_blank"><img src="assets/images/icons/github-link.png" alt="GitHub: luisjosecruz"></a></li>
+            <!-- <li class="social-media__item"><a href="#" target="_blank"><img src="assets/images/icons/linkedin.png" alt="LikedIn: luisjosecruz"></a></li> -->
+            <li class="social-media__item"><a href="https://twitter.com/luismartmusic" target="_blank"><img src="assets/images/icons/twitter.png" alt="Twitter: luismartmusic"></a></li>
+            <li class="social-media__item"><a href="https://www.facebook.com/luismartmusic" target="_blank"><img src="assets/images/icons/facebook.png" alt="Facebook: luismartmusic"></a></li>
+            <li class="social-media__item"><a href="https://www.instagram.com/luismartmusic/" target="_blank"><img src="assets/images/icons/instagram.png" alt="Instagram: luismartmusic"></a></li>
+            <li class="social-media__item"><a href="https://www.youtube.com/channel/UCqkxFr53JmfLi_NiteA8HHA" target="_blank"><img src="assets/images/icons/youtube.png" alt="Youtube: clarkode"></a></li>
+        </ul>
+        <div class="footer__text">
+            <p>Copyright © Luis Cruz 2021</p>
+        </div>
+        <a href="#" class="gohome"><img src="assets/images/icons/arrow.png" alt="Go to home page link"></a>
+    </footer>
+</body>
+</html>
